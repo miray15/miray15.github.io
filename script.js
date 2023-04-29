4444235a284fdd6f68f4495de00ac43635ca1945
Miray Karabulut <mimikarabulut@gmail.com>
2023-04-29 18:06:15 -0500

    initialize like count at 351

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-let likeCount = 21;
+// let likeCount = 21;
 
-function likeImage() {
-  likeCount++;
-  document.querySelector('.like-count').textContent = likeCount;
+// function likeImage() {
+//   likeCount++;
+//   document.querySelector('.like-count').textContent = likeCount;
+// }
+
+window.onload = function() {
+  let likeCount = 351; // initialize like count to 100
+  document.getElementById('like-count').textContent = likeCount;
+  
+  document.getElementById('like-button').addEventListener('click', function() {
+    likeCount++;
+    document.getElementById('like-count').textContent = likeCount;
+  });
 }
